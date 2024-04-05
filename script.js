e410ad37983078b20a8878e85a3488475a31e5c6
novilianaramadani <avi.djabar@gmail.com>
2024-04-05 15:48:36 +0800

    v1.3 - Nomor 5 sampai 9

--- script.js
+++ script.js
@@ -4,4 +4,27 @@ let buku = [
     {nama: 'aladin', jumlah: 2},
 ];
 
+function cekPeminjaman() {
+    let jumlah = parseInt(document, getElementById('jumlah').value);
+    let bukuTerpilih = document.getElementById('buku').value;
 
+    if (jumlah === 0 || isNaN(jumlah)) {
+        alert('Masukkan jumlah buku yang valid.');
+    } else {
+        let bukuDitemukan = false;
+        for (let i = 0; i < buku.length; i++) {
+            if (buku[i].nama === bukuTerpilih) {
+                bukuDitemukan = true;
+                if (jumlah > buku[i].jumlah) {
+                    alert("Jumlah buku tidak tersedia.");
+                } else {
+                    alert("Silahkan ambil buku anda.");
+                }
+                break;
+            }
+        }
+        if (!bukuDitemukan) {
+            alert("Buku tidak ditemukan");
+        }
+    }
+}
